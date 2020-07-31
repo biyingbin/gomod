7cce0313066aaea8f3ba36bcaf76e0ff483e4851
byb <laobi_1989@126.com>
2020-07-31 14:20:18 +0800

    go-redis client

--- main/redis.go
+++ main/redis.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/go-redis/redis"
 	"sync"
-	"time"
 )
 
 var clusterClient *redis.ClusterClient
@@ -17,33 +16,47 @@ func main() {
 
 	wait.Add(50)
 
-	go func() {
+	for i := 0; i < 300; i++ {
 
-		// 连接redis集群
-		client := redis.NewClusterClient(&redis.ClusterOptions{
-			Addrs: []string{ // 填写master主机
-				//"172.16.1.2:7000",
-				"172.16.1.2:7001",
-				//"172.16.1.2:7002",
-			},
-			Password:     "",               // 设置密码
-			DialTimeout:  50 * time.Second, // 设置连接超时
-			ReadTimeout:  50 * time.Second, // 设置读取超时
-			WriteTimeout: 50 * time.Second, // 设置写入超时
-		})
-		// 发送一个ping命令,测试是否通
-		pong, err := client.Ping().Result()
-		fmt.Println(pong, err)
+		go func() {
 
-		for {
-			get := client.Get("aaaa")
+			// 连接redis集群
+			//client := redis.NewClusterClient(&redis.ClusterOptions{
+			//	Addrs: []string{ // 填写master主机
+			//		//"172.16.1.2:7000",
+			//		"172.16.1.2:6379",
+			//		//"172.16.1.2:7002",
+			//	},
+			//	Password:     "",               // 设置密码
+			//	DialTimeout:  50 * time.Second, // 设置连接超时
+			//	ReadTimeout:  50 * time.Second, // 设置读取超时
+			//	WriteTimeout: 50 * time.Second, // 设置写入超时
+			//})
 
-			_ = get
-			//fmt.Println(get.Result())
-			//fmt.Println("=======")
-		}
+			// 发送一个ping命令,测试是否通
+			client := redis.NewClient(&redis.Options{
+				Addr:     "172.16.1.2:6379",
+				Password: "", // no password set
+				DB:       0,  // use default DB
+			})
 
-	}()
+			for {
+
+				get := client.MGet("fjklajdfljfljaf")
+
+				_ = get //.Result()
+
+				//if (err != nil) {
+				//	fmt.Println(err)
+				//}
+
+				//fmt.Println(get.Result())
+				//fmt.Println("=======")
+			}
+
+		}()
+
+	}
 
 	wait.Wait()
 
